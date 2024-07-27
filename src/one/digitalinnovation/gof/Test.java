b23b51f46682133b0a8aee29c7cf3b0e7eb9db6b
o0Juh0o <juliana.gs.miguel@gmail.com>
2024-07-27 18:26:24 -0300

    Att final

---
+++ src/one/digitalinnovation/gof/Test.java
@@ -0,0 +1,32 @@
+package one.digitalinnovation.gof;
+
+import one.digitalinnovation.gof.singleton.SingletonEager;
+import one.digitalinnovation.gof.singleton.SingletonLazy;
+import one.digitalinnovation.gof.singleton.SingletonLazyHolder;
+
+public class Test {
+
+	public static void main(String[] args) {
+	//Singleton
+	
+	SingletonLazy lazy = SingletonLazy.getInstancia();
+	System.out.println(lazy);
+	lazy = SingletonLazy.getInstancia();
+	System.out.println(lazy);
+	
+	SingletonEager eager = SingletonEager.getInstancia();
+	System.out.println(eager);
+	eager = SingletonEager.getInstancia();
+	System.out.println(eager);
+	
+	SingletonLazyHolder lazyHolder = SingletonLazyHolder.getInstancia();
+	System.out.println(lazyHolder);
+	lazyHolder = SingletonLazyHolder.getInstancia();
+	System.out.println(lazyHolder);
+	
+	//Strategy 
+	
+	Comportamento defensivo = new ComportamentoDefensivo();
+	Comportamento normal = new ComportamentoNormal(); 
+	Comportamento agressivo = new ComporamentoAgressivo();
+}
---
+++ src/one/digitalinnovation/gof/strategy/ComportamentoAgressivo.java
@@ -0,0 +1,11 @@
+package one.digitalinnovation.gof.strategy;
+
+public class ComportamentoAgressivo implements Comportamento {
+
+	public void mover() {
+		System.out.println("Movendo-se agressivamente...");
+	}
+	
+		
+
+}
